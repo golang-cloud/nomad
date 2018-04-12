654543483b8b8a6550ee5a040f3e952fc5ce8008
chenbing <chenjpu@gmail.com>
2018-04-12 20:46:43 +0800

    同步接口修改，解决编译错误

--- nomad/endpoints_oss.go
+++ nomad/endpoints_oss.go
@@ -5,15 +5,17 @@ package nomad
 import "net/rpc"
 
 // EnterpriseEndpoints holds the set of enterprise only endpoints to register
-type EnterpriseEndpoints struct{}
+type EnterpriseEndpoints struct {
+	s *Server
+}
 
 // NewEnterpriseEndpoints returns a stub of the enterprise endpoints since there
 // are none in oss
 func NewEnterpriseEndpoints(s *Server) *EnterpriseEndpoints {
-	return &EnterpriseEndpoints{}
+	return &EnterpriseEndpoints{s}
 }
 
 // Register is a no-op in oss.
-func (e *EnterpriseEndpoints) Register(s *Server) {
-	s.rpcServer.Register(&Namespace{s})
+func (e *EnterpriseEndpoints) Register(s *rpc.Server) {
+	s.Register(&Namespace{e.s})
 }
